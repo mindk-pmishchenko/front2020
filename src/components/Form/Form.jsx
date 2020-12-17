8f1420574a1681df422bc90b5a2642dac002c067
Pavel Mishchenko <pmishchenko@mindk.com>
2020-12-17 20:32:01 +0200

    lesson 4, 17/12/20

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,39 @@
+import PropTypes from 'prop-types';
+
 import Text from './components/Text/Text';
 
-function Form({title, size, onSubmit}) {
+function Form({title, size, onSubmit, counter, initialValues, userData}) {
     const initialInputValue = 'Test value';
     onSubmit('Submitted form data!');
+
+    throw new Error('Some error!');
+
 
     return (
         <div>
             {title}
+            counter: {counter + 1}
             {size && <Text inputSize={size} initial={initialInputValue}/>}
         </div>
     );
 }
+
+const sizes = [5, 10, 20, 30, 40];
+const initialValuesType = PropTypes.shape({
+    title: PropTypes.string,
+    size: PropTypes.number
+});
+
+Form.propTypes = {
+    title: PropTypes.string.isRequired,
+    size: PropTypes.oneOf(sizes),
+    onSubmit: PropTypes.func.isRequired,
+    counter: PropTypes.number,
+    initialValues: initialValuesType
+};
+
+Form.defaultProps = {
+    counter: 1
+};
 
 export default Form;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,5 +1,6 @@
 import Form from '../Form/Form';
 import React, {useState} from 'react';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 function UserPage({onSubmit, initialData}) {
     const fromTitle = 'User creation form';
@@ -16,9 +17,44 @@ function UserPage({onSubmit, initialData}) {
         setCounter(counter - 1);
     };
 
+    const initialValues = {
+        title: 'New User',
+        size: 10
+    };
+
+    const userData = {
+        id: 1,
+        firstName: 'Ivan',
+        lastName: 'Ivanov',
+        age: 25,
+        avatar: {
+            fileId: 1,
+            file: {
+                id: 1,
+                name: 'photo.jpg',
+                path: '/upload/photo.jpg',
+                size: 1234
+            }
+        },
+        friends: [{}, {}, {}], //array of users
+        articles: [{
+            title: 'Article 1',
+            text: 'Some text',
+            images: [{}, {}, {}], // array of files
+            createdAt: '2020-12-17 19:00:00',
+            editedAt: '2020-12-17 20:00:00',
+            likes: [
+                {userId: 2, user: {id: 2}, date: '2020-12-17 21:00:00'},
+                {userId: 3, user: {id: 3}, date: '2020-12-17 22:00:00'}
+            ]
+        }]
+    };
+
     return (
         <>
-            <Form title={fromTitle} size={inputSize} onSubmit={onSubmit} counter={counter}/>
+            <ErrorBoundary>
+                <Form title={fromTitle} size={inputSize} onSubmit={onSubmit} initialValues={initialValues} userData={userData}/>
+            </ErrorBoundary>
             <button type="button" onClick={handleClickPlus}>Click+</button>
             <button type="button" onClick={handleClickMinus}>Click-</button>
         </>
